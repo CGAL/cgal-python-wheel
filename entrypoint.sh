136dbd7d65c424b0874a40e0bc4b6aa3e9e203ec
Maxime Gimeno <maxime.gimeno@gmail.com>
2020-07-27 14:37:00 +0200

    Bump version number

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 cd /cgal-bindings && python3 setup.py bdist_wheel --cgal-dir=/cgal --boost-dir=/usr/include/boost169 --boost-serialization-lib=/usr/lib64/libboost_serialization.so.1.69.0 --boost-iostreams-lib=/usr/lib64/libboost_iostreams.so.1.69.0 \
-    --boost-regex-lib=/usr/lib64/libboost_regex.so.1.69.0
-auditwheel repair dist/cgal-5.0.2-cp36-cp36m-linux_x86_64.whl
+    --boost-regex-lib=/usr/lib64/libboost_regex.so.1.69.0 --keep-temp
+auditwheel repair dist/cgal-5.1.0-cp36-cp36m-linux_x86_64.whl
