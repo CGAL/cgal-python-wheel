7ed92fde967420a21fa94cb8c5541c37c171bd16
Maxime Gimeno <maxime.gimeno@gmail.com>
2020-07-27 16:28:39 +0200

    Don't specify wheel name in auditwheel call

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@ set -e
 PYTHON_ROOT_DIR=/opt/python/cp3${PYTHON_VERSION_MINOR}-cp3${PYTHON_VERSION_MINOR}
 cd /cgal-bindings && ${PYTHON_ROOT_DIR}/bin/python3 setup.py bdist_wheel --cgal-dir=/cgal --boost-dir=/usr/include/boost169 --boost-serialization-lib=/usr/lib64/libboost_serialization.so.1.69.0 --boost-iostreams-lib=/usr/lib64/libboost_iostreams.so.1.69.0 \
     --boost-regex-lib=/usr/lib64/libboost_regex.so.1.69.0 --keep-temp --python-root=${PYTHON_ROOT_DIR}
-auditwheel repair dist/cgal-5.1.0-cp36-cp36m-linux_x86_64.whl
+auditwheel repair dist/*
