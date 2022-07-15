320e450e345c0b3cfec69b7cdfbe8209500c9c9f
Laurent Rineau <laurent.rineau@cgal.org>
2022-07-15 11:29:18 +0200

    Fix the wheel creation

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-set -e
+set -ex
 /usr/local/bin/python3.${PYTHON_VERSION_MINOR} -m pip install --upgrade pip
+mkdir -p /cgal-bindings/build/build-python/CGAL
 cd /cgal-bindings && BOOST_INCLUDEDIR=/usr/include/boost169 CGAL_DIR=/cgal /usr/local/bin/python3.${PYTHON_VERSION_MINOR} -m pip wheel -v .
-auditwheel repair *.whl
+LD_LIBRARY_PATH=$PWD/build/lib auditwheel repair cgal-*.whl
