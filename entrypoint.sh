c3b8ec3a76753f1dc5d82b8d9c4a986e42ca68f7
Laurent Rineau <laurent.rineau@cgal.org>
2022-07-15 15:39:08 +0200

    Update the entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,16 @@
 set -ex
 /usr/local/bin/python3.${PYTHON_VERSION_MINOR} -m pip install --upgrade pip
 mkdir -p /cgal-bindings/build/build-python/CGAL
+if [ -f /cgal/CGALConfig.cmake ]; then
+  CGAL_DIR=/cgal
+else
+#  shopt
+  ls /cgal
+  ls /cgal/lib*/cmake/CGAL/CGALConfig.cmake
+  files=(/cgal/lib*/cmake/CGAL/CGALConfig.cmake)
+  echo $files
+  file=${files[0]}
+  CGAL_DIR=${file%\/CGALConfig.cmake}
+fi
 cd /cgal-bindings && BOOST_INCLUDEDIR=/usr/include/boost169 CGAL_DIR=/cgal /usr/local/bin/python3.${PYTHON_VERSION_MINOR} -m pip wheel -v .
 LD_LIBRARY_PATH=$PWD/build/lib auditwheel repair cgal-*.whl
