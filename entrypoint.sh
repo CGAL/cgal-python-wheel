c641133277c474972883bf433d06a49f59f9d3ba
Laurent Rineau <laurent.rineau@cgal.org>
2022-07-24 14:10:34 +0200

    Fix the use of Boost

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,5 +14,5 @@ else
   CGAL_DIR=${file%\/CGALConfig.cmake}
 fi
 export CGAL_DIR
-cd /cgal-bindings && BOOST_INCLUDEDIR=/usr/include/boost169 /usr/local/bin/python3.${PYTHON_VERSION_MINOR} -m pip wheel -v .
-LD_LIBRARY_PATH=$PWD/build/lib auditwheel repair cgal-*.whl
+cd /cgal-bindings && BOOST_LIBRARYDIR=/usr/lib64/boost169 BOOST_INCLUDEDIR=/usr/include/boost169 /usr/local/bin/python3.${PYTHON_VERSION_MINOR} -m pip wheel -v .
+LD_LIBRARY_PATH=$PWD/build/lib:/usr/lib64/boost169 auditwheel repair cgal-*.whl
